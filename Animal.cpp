d719af6c9f83f771ad06b5cf12dd3d8c9140ee9e
MichalKazm <michal.kazmierowski2005@gmail.com>
2025-04-19 19:46:44 +0200

    Create class Animal

---
+++ Animal.cpp
@@ -0,0 +1,57 @@
+#include "Animal.h"
+
+#include <cstdlib>
+
+#include "World.h"
+
+Animal::Animal(int strength, int initiative, int x, int y, char symbol, World *world)
+    : Organism(strength, initiative, x, y, symbol, world) {}
+void Animal::Action() {
+    int direction = -1;
+
+    // Move in a random available direction
+    while (direction < 0) {
+        direction = std::rand() % 4;
+
+        switch (direction) {
+            // Move up
+            case 0:
+                if (y > 0) {
+                    y--;
+                }
+                else {
+                    direction = -1;
+                }
+                break;
+            // Move down
+            case 1:
+                if (y < world->GetRows() - 1) {
+                    y++;
+                }
+                else {
+                    direction = -1;
+                }
+                break;
+            // Move left
+            case 2:
+                if (x > 0) {
+                    x--;
+                }
+                else {
+                    direction = -1;
+                }
+            // Move right
+            case 3:
+                if (x < world->GetCols() - 1) {
+                    x++;
+                }
+                else {
+                    direction = -1;
+                }
+                break;
+        }
+    }
+
+    age++;
+}
+
---
+++ Animal.h
@@ -0,0 +1,14 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#include "Organism.h"
+
+class Animal : public Organism {
+public:
+    Animal(int strength, int initiative, int x, int y, char symbol, World* world);
+
+    void Action() override;
+
+    ~Animal() override = default;
+};
+
+#endif
